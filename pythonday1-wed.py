6894bc6e05b2cfeb060fa9c75d84b8e9a467b55c
Ishwarya <moriyaishu2244@gmail.com>
2025-12-17 20:09:02 +0530

    Add files via upload
    Today i started my journey to practice python everyday ...

---
+++ pythonday1-wed.py
@@ -0,0 +1,61 @@
+
+# PYTHON DAY 1- WED(17/12/2025)
+
+#splitting
+name="A-i-s-h-u"
+splitting=name.split("*")
+print(splitting)
+
+color="red"
+justname=color.split(",")
+print(justname)
+
+girl="A-m-m-u"
+dash=girl.split("-")
+print(dash)
+
+boy="r/a/j"
+slash=boy.split("/")
+print(slash)
+
+
+#the default separator when using split() ..
+
+
+lemon="trick-9 loo ero"
+sel=lemon.split()
+print(sel)
+
+#Replace
+dot="Aish."
+rep=dot.replace(".","!!")
+print(rep)
+
+release_date="Release date:25 june"
+repl=release_date.replace("25 june","wed december-2025")
+print(repl)
+
+girls="divya + shivanya"
+andd=girls.replace("+","and")
+print(andd)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
