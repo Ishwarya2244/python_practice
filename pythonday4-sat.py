dc421e415320bdb480caeb53d089fe84a90efb5a
Ishwarya <moriyaishu2244@gmail.com>
2025-12-20 20:45:58 +0530

    Add files via upload
    python practiced - sat

---
+++ pythonday4-sat.py
@@ -0,0 +1,17 @@
+#plotting a displot(distribution plot)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.displot([0,1,2,3,4,5])
+plt.show()
+
+
+#plotting a displot without the histogram
+
+import matplotib.pyplot as plt
+import seaborn as sns
+sns.displot([0,1,2,3,4,5,6],kind="kde")
+plt.show()
+
+
+
