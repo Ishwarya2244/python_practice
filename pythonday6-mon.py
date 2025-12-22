15a69df6aa6ad1dc26538e07f5f81d663f87176e
Ishwarya <moriyaishu2244@gmail.com>
2025-12-22 20:46:21 +0530

    Add files via upload
    add monday python practiced file

---
+++ pythonday6-mon.py
@@ -0,0 +1,54 @@
+name="Ishu"
+age=20
+cgpa=8.5
+is_student=True
+print(name)
+print(age)
+print(cgpa)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
+
+age="20"
+print(type(age))
+
+name=input("Enter your name:")
+age=input("Enter your age:")
+print(name)
+print(age)
+print(type(age)) #str
+
+age=int(input("Enter age:"))
+print(type(age))#int
+
+
+a=input("enter your first number:")
+b=input("enter your second number:")
+print(a+b)
+
+
+x="10"
+y=5
+print(x*y)
+
+a=int("20")
+b=input()
+print(a+b)
+
+
+num1=int(input("enter a number:"))
+print(type(num1))
+
+
+
+
+
+
+
+
+
+
+
