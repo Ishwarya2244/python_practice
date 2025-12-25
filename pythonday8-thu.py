1a173d5bac2137007225c66c24b92f7605b19d16
Ishwarya <moriyaishu2244@gmail.com>
2025-12-25 19:53:24 +0530

    Add files via upload
    add python practiced thursday

---
+++ pythonday8-thu.py
@@ -0,0 +1,65 @@
+for i in range(1,6):
+    print(i)
+for i in range(6):
+    print(i)
+for i in range(1,6):
+    print(i*i)
+for i in range(3):
+    print(i*i)
+
+n=int(input("Enter a number:"))
+for i in range(1,n+1):
+    print(i)
+
+n=int(input("enter your number:"))
+for i in range(1,n+1):
+    if i % 2==0:
+        print(i)
+
+num=int(input("enter number:"))
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+num1=int(input("enter num1:"))
+print(num1**2)
+
+n1=2
+n2=5
+if n1 >= n2:
+    print(n1)
+else:
+    print(n2)
+
+num=int(input("enter a number:"))
+for i in range(1,num):
+    print(i)
+
+n=4
+if n>0:
+    print("positive")
+elif n<0:
+    print("negative")
+else:
+    print("zero")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
