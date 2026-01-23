91295d9a0484675b9050bf156588b4485000a24a
Ishwarya <moriyaishu2244@gmail.com>
2026-01-23 20:39:50 +0530

    Add files via upload

---
+++ day10,jan-23.py
@@ -0,0 +1,21 @@
+for i in range(1,11):
+    if i==6:
+        break
+    print(i)
+
+for i in range(1,11):
+    if i==5:
+        continue
+    print(i)
+
+for i in range(1,6):
+    if i==3:
+        pass
+    print(i)
+
+for i in range(1,6):
+    if i %2==0:
+        continue
+    print(i)
+
+
